1ad609f41bec830d113af809203b5cc833c38e42
Musab2077 <muhammadmusab2077@gmail.com>
2025-07-13 15:53:31 +0500

    Developing saving and acessing routers

--- backend/routers/chat.py
+++ backend/routers/chat.py
@@ -11,9 +11,6 @@ from modules.new_chat_tb import NewChat
 from modules.saving_chats_tb import Chat
 import os
 
-dotenv_path = find_dotenv()
-
-x = load_dotenv(dotenv_path)
 
 chat_router = APIRouter()
 
@@ -22,62 +19,78 @@ class BotResponse(BaseModel):
     email_id : int
     chat_id : int
 
-index_name = "midster-bot"
-embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
+def rendering_bot():
+    try:
+        index_name = "midster-bot"
+        embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
 
-PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+        PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+        GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+        dotenv_path = find_dotenv()
 
-embeddings=HuggingFaceEmbeddings(model_name= embedding_model_name)
-docsearch = PineconeVectorStore.from_existing_index(
-    index_name = index_name,
-    embedding = embeddings,
-)
+        x = load_dotenv(dotenv_path)
 
-retriever = docsearch.as_retriever(search_type = 'similarity', search_kwargs = {'k':3})
-llm = ChatGroq(model_name = 'llama-3.3-70b-versatile',groq_api_key = GROQ_API_KEY)
+        embeddings=HuggingFaceEmbeddings(model_name= embedding_model_name)
+        docsearch = PineconeVectorStore.from_existing_index(
+            index_name = index_name,
+            embedding = embeddings,
+        )
 
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. keep the "
-    "answer detailed."
-    "\n\n"
-    "{context}"
-)
+        retriever = docsearch.as_retriever(search_type = 'similarity', search_kwargs = {'k':3})
+        llm = ChatGroq(model_name = 'llama-3.3-70b-versatile',groq_api_key = GROQ_API_KEY)
+        system_prompt = (
+            "You are an assistant for question-answering tasks. "
+            "Use the following pieces of retrieved context to answer "
+            "the question. If you don't know the answer, say that you "
+            "don't know. keep the "
+            "answer detailed."
+            "\n\n"
+            "{context}"
+        )
 
-system_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-)
+        system_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system_prompt),
+                ("human", "{input}"),
+            ]
+        )
 
-question_answer_chain = create_stuff_documents_chain(llm, system_prompt)
-rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        question_answer_chain = create_stuff_documents_chain(llm, system_prompt)
+        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+        return llm, rag_chain
+    except:
+        return "error"
+
+output = rendering_bot()
 
 @chat_router.post('/bot_responses')
 async def bot_responses(bot:BotResponse):
-    med_response = rag_chain.invoke({"input": bot.message})
-    def context_prompt(type_of_response):
-        context_prompt = [
-            ("system",
-            "you are an assistant that make the big statements into small and consise"
-            "I give you the statement and you have to make it into three words"
-            ),
-            ('human',type_of_response)]
-        return context_prompt
-    human_context = llm.invoke(context_prompt(bot.message)) # for making the human messages context
-    bot_context = llm.invoke(context_prompt(med_response["answer"])) # for making the bot generated messages context
+    try:
+        if output == "error":
+            return {"response" : "Server is busy."}
+        
+        llm, rag_chain = output
+        med_response = rag_chain.invoke({"input": bot.message})
+        def context_prompt(type_of_response):
+            context_prompt = [
+                ("system",
+                "you are an assistant that make the big statements into small and consise"
+                "I give you the statement and you have to make it into three words"
+                ),
+                ('human',type_of_response)]
+            return context_prompt
+        human_context = llm.invoke(context_prompt(bot.message)) # for making the human messages context
+        bot_context = llm.invoke(context_prompt(med_response["answer"])) # for making the bot generated messages context
 
-    # Saving in the DB
-    if bot.chat_id == 0:
-        new_chat = await NewChat.create(email_id = bot.email_id)
-        human_chat = await Chat.create(response="human",context=human_context.content,message=bot.message, chat_id=new_chat.pk) # For saving human messages
-        bot_chat = await Chat.create(response="bot",context=bot_context.content,message=med_response['answer'], chat_id=new_chat.pk) # For saving bot messages
-        return {'response' : med_response['answer'], "chat_id" : bot_chat.chat_id}
-    else:
-        human_chat = await Chat.create(response="human",context=human_context.content,message=bot.message, chat_id=bot.chat_id) # For saving human messages
-        bot_chat = await Chat.create(response="bot",context=bot_context.content,message=med_response['answer'], chat_id=bot.chat_id) # For saving bot messages
-        return {'response' : med_response['answer'], "chat_id" : bot_chat.chat_id}
+        # Saving in the DB
+        if bot.chat_id == 0:
+            new_chat = await NewChat.create(email_id = bot.email_id)
+            human_chat = await Chat.create(response="human",context=human_context.content,message=bot.message, chat_id=new_chat.pk) # For saving human messages
+            bot_chat = await Chat.create(response="bot",context=bot_context.content,message=med_response['answer'], chat_id=new_chat.pk) # For saving bot messages
+            return {'response' : med_response['answer'], "chat_id" : bot_chat.chat_id}
+        else:
+            human_chat = await Chat.create(response="human",context=human_context.content,message=bot.message, chat_id=bot.chat_id) # For saving human messages
+            bot_chat = await Chat.create(response="bot",context=bot_context.content,message=med_response['answer'], chat_id=bot.chat_id) # For saving bot messages
+            return {'response' : med_response['answer'], "chat_id" : bot_chat.chat_id}
+    except:
+        return {"response" : "An error Ocurred"}
---
+++ backend/routers/loading_chats.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from modules.saving_chats_tb import Chat
+from modules.new_chat_tb import NewChat
+
+loading_chats = APIRouter()
+
+class Ids(BaseModel):
+    email_id : int
+
+class ButtonIds(BaseModel):
+    id : int
+
+@loading_chats.post('/loading_chats')
+async def loading_chat(arg:Ids):
+    newchats = await NewChat.filter(email_id=arg.email_id)
+    button_keys = []
+    for newchat in newchats:
+        chat = await Chat.filter(chat_id=newchat.pk)
+        chat = chat[1]
+        button_keys.append({"chat_id":newchat.pk,"context":chat.context})
+
+    button_keys.reverse()
+    return button_keys
+
+@loading_chats.post('/accessing_chat')
+async def accessing_chats(arg :ButtonIds):
+    chats = await Chat.filter(chat_id = arg.id)
+    human_chats = chats[0::2]
+    bot_chats = chats[1::2]
+    instance = []
+    for i in range(len(bot_chats)):
+        human_row = await human_chats[i].get(id=human_chats[i].pk)
+        human_response = human_row.response
+        human_message = human_row.message
+        
+        bot_row = await bot_chats[i].get(id=bot_chats[i].pk)
+        bot_response = bot_row.response
+        bot_message = bot_row.message
+        instance.append({human_response : human_message, bot_response: bot_message})
+    return list(instance)
