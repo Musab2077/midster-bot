edc927fa2f434c01ca023c8609df029c62a7f7f9
Musab2077 <muhammadmusab2077@gmail.com>
2025-07-27 22:53:10 +0500

    fixed the existing chat error of individual

--- backend/routers/loading_chats.py
+++ backend/routers/loading_chats.py
@@ -4,12 +4,10 @@ from modules.saving_chats_tb import Chat
 from modules.new_chat_tb import NewChat
 
 loading_chats = APIRouter()
-
-class Ids(BaseModel):
+    
+class Accessing(BaseModel):
     email_id : int
-
-class ButtonIds(BaseModel):
-    id : int
+    chat_id : int
 
 @loading_chats.get('/loading_chats/{email_id}')
 async def loading_chat(email_id):
@@ -26,22 +24,33 @@ async def loading_chat(email_id):
         return button_keys
     return button_keys
 
-@loading_chats.get('/accessing_chat/{id}')
-async def accessing_chats(id):
-    chats = await Chat.filter(chat_id = id)
-    human_chats = chats[0::2]
-    bot_chats = chats[1::2]
-    instance = []
-    for i in range(len(bot_chats)):
-        human_row = await human_chats[i].get(id=human_chats[i].pk)
-        human_response = human_row.response
-        human_message = human_row.message
-        
-        bot_row = await bot_chats[i].get(id=bot_chats[i].pk)
-        bot_response = bot_row.response
-        bot_message = bot_row.message
-        instance.append({human_response : human_message, bot_response: bot_message})
-    return list(instance)
+@loading_chats.post('/accessing_chat')
+async def accessing_chats(arg: Accessing):
+    if arg.chat_id > 0:
+        chat = await Chat.exists(chat_id = arg.chat_id)
+    if chat:
+        chats = await Chat.filter(chat_id = arg.chat_id)
+        newchat = await NewChat.get(pk = arg.chat_id)
+        if newchat.email_id == arg.email_id:
+            human_chats = chats[0::2]
+            bot_chats = chats[1::2]
+            context = bot_chats[0].context
+            instance = []
+            for i in range(len(bot_chats)):
+                human_row = await human_chats[i].get(id=human_chats[i].pk)
+                human_response = human_row.response
+                human_message = human_row.message
+                
+                bot_row = await bot_chats[i].get(id=bot_chats[i].pk)
+                bot_response = bot_row.response
+                bot_message = bot_row.message
+                instance.append({human_response : human_message, bot_response: bot_message})
+            return {"exist" : True,
+                    "response" : list(instance),
+                    "context" : context}
+        else:
+            return {"exist" : False}
+    return {"exist" : False}
 
 @loading_chats.delete('/deleting_chat/{id}')
 async def deleting_the_chat(id):
